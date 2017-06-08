b49445b03733b3a72982fe92cb7da2b1e4dbc411
alberto-guzman <alvarez1991@gmail.com>
2017-06-08 15:15:54 -0400

    added week 4 code

--- PythonCode/temp_conv.py
+++ PythonCode/temp_conv.py
@@ -3,12 +3,22 @@
 # show for  for 0 C to 100 C
 # F = 9/5C+32
 
+# print(format('C', '>4s'), format('F', '>10s'), sep='')
+
+
+# temp_C = 0
+
+#  while temp_C <= 100:
+# temp_F = 9 / 5 * temp_C + 32
+# print(format(temp_C, '4d'), format(temp_F, '6.2f'), sep='')
+# temp_C += 5
+
+
+# converting using loops
+
 print(format('C', '>4s'), format('F', '>10s'), sep='')
 
-
-temp_C = 0
-
-while temp_C <= 100:
+for temp_C in range(0, 101, 5):
     temp_F = 9 / 5 * temp_C + 32
-    print(format(temp_C, '4d'), format(temp_F, '6.2f'), sep='')
-    temp_C += 5
+    print(format(temp_C, '4d'), format(temp_F, '>10.2f'), sep='')
+#
